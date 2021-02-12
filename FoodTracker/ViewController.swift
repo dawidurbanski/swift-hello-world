cdf59a5ceed01818a749fb02e02c10f89b6df0bf
Dawid Urbański <hi@dawidurbanski.com>
2021-02-12 13:33:16 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Dawid Urbański on 2/12/21.
+//  Copyright © 2021 Dawid Urbański. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
